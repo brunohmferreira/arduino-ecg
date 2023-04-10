2a39226639a8ce458f806ca0e5b1c11141100ad2
Bruno Ferreira <68091873+brunohmferreira@users.noreply.github.com>
2023-04-10 10:54:05 -0300

    Add files via upload

---
+++ ECGECG.ino
@@ -0,0 +1,63 @@
+//grafico no reajusta mais
+//grafico mais bonito
+//buzzermais grave
+//comprar modulo e cartao SD para salvar os dados, biblioteca SD.h
+//falta imprimir os valores dos batimentos
+#include <SD.h>
+
+Sd2Card SDcard;
+SdVolume volume;
+
+
+int LODminus = 10;//LOD-
+int LODplus = 11;//LOD+
+int buzzer = 13;
+const int chipSelect = 4; 
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LODminus, INPUT); // leads off detection LOD-
+  pinMode(LODplus, INPUT); // leads off detection LOD+
+  pinMode(buzzer, OUTPUT);
+  if (!SD.begin(chipSelect)) 
+  {
+    Serial.println("Falha ao acessar o cartao !");
+    return;
+  }
+}
+
+
+
+void loop()
+{
+  Serial.print(analogRead(A0));//sensor lecture
+  Serial.print(" ");
+  Serial.print(470);//formatacao do grafico - acima
+  Serial.print(" ");
+  Serial.println(270);//formatacao do grafico - abaixo
+
+  File dataFile = SD.open("ECG_values.txt", FILE_WRITE);
+  if (dataFile) // Grava os dados no arquivo
+    {
+      dataFile.println(analogRead(A0));
+      dataFile.close();
+    }  
+  else 
+    {
+      Serial.println("Erro ao abrir arquivo.txt !"); // Mensagem de erro caso ocorra algum problema na abertura do arquivo
+    } 
+
+  if (analogRead(A0) > 360) { //pulse
+      tone(buzzer, 63);//buzzer 63 Hertz
+  }
+  else {
+      noTone(buzzer);//buzzer off
+  }
+}
+
+
+
+
+
---
+++ ECG_Bruno.ino
@@ -0,0 +1,59 @@
+int LED = 8;//if any electrode is disconnected
+int LODminus = 10;//LOD-
+int LODplus = 11;//LOD+
+int button = 12; //mute mode
+int buzzer = 13; 
+
+
+int state = 0; //mute mode off
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(LODminus, INPUT); // leads off detection LOD-
+  pinMode(LODplus, INPUT); // leads off detection LOD+
+  pinMode(buzzer, OUTPUT); 
+  pinMode(LED, OUTPUT); 
+  pinMode(button, INPUT_PULLUP); 
+}
+
+
+
+void loop() 
+{
+  if((digitalRead(LODminus) == 1)||(digitalRead(LODplus) == 1)){//if any electrode is disconnected
+   Serial.println("ERROR:electrode is disconnected");
+   digitalWrite(LED, HIGH);//electrode disconnected alert
+  }
+  else if(digitalRead(LODplus) == 1){
+    Serial.println("ERROR: LA electrode is disconnected");
+    digitalWrite(LED, HIGH);//electrode disconnected alert
+  }
+  else{//if everything is ok
+    digitalWrite(LED, LOW);
+    Serial.println(analogRead(A0));//sensor lecture
+     if(analogRead(A0) > 400){//pulse
+       if(state == 0){//mute mode off
+         //digitalWrite(buzzer, HIGH);
+       }
+     }
+     else{
+       if(state == 0){//mute mode off
+        //digitalWrite(buzzer, LOW);
+       }
+     }
+  }
+  delay(50);
+
+  //if (digitalRead(button) == LOW){
+   // if(state == 0){//mute mode off
+    //  digitalWrite(buzzer, LOW);
+    //  state = 1;
+   // }
+   // else if(state == 1){//mute mode on
+   //   digitalWrite(buzzer, HIGH);
+   //   state = 0;
+  //  }
+  //  delay(500);
+  //}
+}
